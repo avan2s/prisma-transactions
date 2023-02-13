00256aeec684a6266e11d028fac25dca43bf6ceb
Andreas van Helden <andreas-van-helden@gmx.de>
2023-02-13 22:05:14 +0100

    add tx client extension for prisma

--- src/annotations/transactional.spec.ts
+++ src/annotations/transactional.spec.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { Prisma, PrismaClient } from "@prisma/client";
+import txPrismaExtension, { FlatTransactionClient } from "../services/prisma-tx-client-extension";
 import { Transactional } from "./transactional";
 
 export class TestClass {
@@ -19,11 +20,11 @@ export class TestClass {
 
 describe('Example Test', () => {
   let toTest = new TestClass();
-  let prismaClient = new PrismaClient({ datasources: { db: { url: 'postgresql://postgres:postgres@localhost:6005/postgres' } }, log: ['query'] })
+  let prismaClient = new PrismaClient({ datasources: { db: { url: 'postgresql://postgres:postgres@localhost:6005/postgres' } }, log: ['query'] }).$extends(txPrismaExtension);
 
   beforeEach(async () => {
     await prismaClient.appUser.deleteMany();
-    await prismaClient.$connect();
+    // await prismaClient.$connect();
   })
 
   afterEach(async () => {
@@ -36,27 +37,34 @@ describe('Example Test', () => {
     const lastName = faker.name.lastName();
     const email = `${firstName}.${lastName}@${faker.internet.domainName()}`;
 
-    // const user = await prismaClient.user.create({data: {
-    // firstName: firstName,
-    //   lastName: lastName,
-    //   email: email
-    // }});
+    const tx = await prismaClient.$begin();
+
+    const user = await tx.appUser.create({data: {
+    firstname: firstName,
+      lastname: lastName,
+      email: email
+    }});
 
     const firstName2 = faker.name.firstName();
     const lastName2 = faker.name.lastName();
     const email2 = `${firstName}.${lastName}@${faker.internet.domainName()}`;
 
-    const user2 = await prismaClient.appUser.create({
+    const user2 = await tx.appUser.create({
       data: {
         firstname: firstName2,
         lastname: lastName2,
         email: email2
       }
     });
+
+    await tx.$commit();
     // await prismaClient.$queryRaw`INSERT INTO app_user(id,firstname,lastname,email) VALUES('foo','peter','klaus','frida@boo.com') RETURNING id`;
     // console.log(user);
     // const userCount = await prismaClient.user.count();
     // toTest.nestedRequiredAnnotationTest();
     expect(1).toBe(1);
   });
+
+  
+
 });
---
+++ src/services/prisma-tx-client-extension.ts
@@ -0,0 +1,71 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+  
+export type FlatTransactionClient = Prisma.TransactionClient & {
+    $commit: () => Promise<void>;
+    $rollback: () => Promise<void>;
+  };
+  
+  const ROLLBACK = { [Symbol.for("prisma.client.extension.rollback")]: true };
+
+
+  const txPrismaExtension =  {
+    client: {
+        async $begin() {
+          const prisma = Prisma.getExtensionContext(this);
+          let setTxClient: (txClient: Prisma.TransactionClient) => void;
+          let commit: () => void;
+          let rollback: () => void;
+    
+          // a promise for getting the tx inner client
+          const txClient = new Promise<Prisma.TransactionClient>((res) => {
+            setTxClient = (txClient) => res(txClient);
+          });
+    
+          // a promise for controlling the transaction
+          const txPromise = new Promise((_res, _rej) => {
+            commit = () => _res(undefined);
+            rollback = () => _rej(ROLLBACK);
+          });
+    
+          // opening a transaction to control externally
+          if (
+            "$transaction" in prisma &&
+            typeof prisma.$transaction === "function"
+          ) {
+            const tx = prisma.$transaction((txClient: Prisma.TransactionClient) => {
+              setTxClient(txClient);
+    
+              return txPromise.catch((e) => {
+                if (e === ROLLBACK) return;
+                throw e;
+              });
+            });
+    
+            // return a proxy TransactionClient with `$commit` and `$rollback` methods
+            return new Proxy(await txClient, {
+              get(target, prop) {
+                if (prop === "$commit") {
+                  return () => {
+                    commit();
+                    return tx;
+                  };
+                }
+                if (prop === "$rollback") {
+                  return () => {
+                    rollback();
+                    return tx;
+                  };
+                }
+                return target[prop as keyof typeof target];
+              },
+            }) as FlatTransactionClient;
+          }
+    
+          throw new Error("Transactions are not supported by this client");
+        },
+      },
+  }
+
+  export default txPrismaExtension;
+  
