94cc71dd99cdef350e75097a7b9ab8152d9f0222
Andreas van Helden <andreas-van-helden@gmx.de>
2023-02-15 18:02:25 +0100

    add rollback test

--- src/annotations/transactional.spec.ts
+++ src/annotations/transactional.spec.ts
@@ -52,6 +52,42 @@ export class TestClass {
     });
     await this.requiredTest(user, prisma);
   }
+
+  public async requiredNestedRollbackTest(
+    foo: string,
+    errorToThrowInNestedMethod: Error,
+    prisma: PrismaClient = this.prismaService
+  ): Promise<void> {
+    const firstname = faker.name.firstName() + foo;
+    const lastname = faker.name.lastName();
+    const email = `${firstname}.${lastname}@${faker.internet.domainName()}`;
+    const user = await prisma.appUser.create({
+      data: {
+        firstname,
+        lastname,
+        email,
+      },
+    });
+    await this.requiredRollbackTest(user, errorToThrowInNestedMethod, prisma);
+  }
+
+  @Transactional({ propagationType: "REQUIRED", prismaClient })
+  public async requiredRollbackTest(
+    user: AppUser,
+    errorToThrow: Error,
+    prisma: PrismaClient
+  ) {
+    const updatedUser = await prisma.appUser.update({
+      where: { id: user.id },
+      data: {
+        firstname: `${user.firstname}-II`,
+      },
+    });
+    if (errorToThrow) {
+      throw errorToThrow;
+    }
+    return updatedUser;
+  }
 }
 
 describe("Example Test", () => {
@@ -101,23 +137,16 @@ describe("Example Test", () => {
     expect(queryEvents[3].query).toBe("COMMIT");
   });
 
-  it(`should rollback the transaction if something failed in the nested method for propagation type REQUIRED`, async () => {
+  it.only(`should rollback the transaction if something failed in the nested method for propagation type REQUIRED`, async () => {
     const queryEvents: Prisma.QueryEvent[] = [];
     prismaClient.$on("query", (event) => queryEvents.push(event));
-    const userArg = {
-      id: 1n,
-      email: "foo@bar.com",
-      firstname: "andy",
-      lastname: "Baum",
-    };
-    await toTest.requiredAttachToTransactionTest("bar");
-    expect(queryEvents.length).toBe(6);
-    expect(queryEvents[0].query).toBe("BEGIN");
-    expect(queryEvents[1].query).toContain("INSERT");
-    expect(queryEvents[2].query).toContain("SELECT");
-    expect(queryEvents[3].query).toContain("INSERT");
-    expect(queryEvents[4].query).toContain("SELECT");
-    expect(queryEvents[5].query).toBe("COMMIT");
+    const expectedError = new Error("some Error");
+    try {
+      await toTest.requiredNestedRollbackTest("bar", expectedError);
+    } catch (err) {
+      const user = await prismaClient.appUser.findFirst();
+      expect(user).toBeUndefined();
+    }
   });
 
   it.skip("should return the expected result", async () => {
