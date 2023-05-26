5acf4fd30ed9879e3adc24ed468ce904096ac9f3
avh <andreas.vanhelden@conceto.de>
2023-05-26 11:01:35 +0200

    rename test

--- src/annotations/transactional.spec.ts
+++ src/annotations/transactional.spec.ts
@@ -417,7 +417,7 @@ describe("Transactional Integration Test", () => {
       expect(queryEvents[3].query).toBe("COMMIT");
     });
 
-    it(`should rollback the first user but should keep the second created user in in new transaction`, async () => {
+    it(`should rollback the first user in REQUIRES_NEW but should keep the second created user in in new transaction`, async () => {
       class TestClass {
         constructor(private prisma: IExtendedPrismaClient) {}
 
@@ -520,6 +520,72 @@ describe("Transactional Integration Test", () => {
         expect(queryEvents[6].query).toBe("COMMIT");
         expect(queryEvents[7].query).toBe("ROLLBACK");
         expect(queryEvents.length).toBe(8);
+
+        expect(await prismaClient.appUser.count()).toBe(1);
+        const user = await prismaClient.appUser.findFirst();
+        expect(user?.firstname).toBe("To");
+        expect(user?.lastname).toBe("Keep");
+      });
+    });
+
+    it.skip(`should rollback the first user in REQUIRED context but should keep 2 created users in REQUIRES NEW context`, async () => {
+      class TestClass {
+        constructor(private prisma: IExtendedPrismaClient) {}
+
+        @Transactional({ propagationType: "REQUIRED", txTimeout: 120000 })
+        public async createUserWithAdditionalUser(): Promise<void> {
+          // this transaction we want to rollback
+          await this.prisma.appUser.create({
+            data: {
+              email: "foo@bar.de",
+              firstname: "John",
+              lastname: "Doe",
+            },
+          });
+          // this is executed in separate transaction and should be kept even after rollback
+          for (let i = 1; i <= 2; i++) {
+            console.log(i);
+            await this.createUserToKeep();
+          }
+
+          throw new Error("some error occured");
+        }
+
+        @Transactional({ propagationType: "REQUIRES_NEW", txTimeout: 120000 })
+        public async createUserToKeep(): Promise<AppUser> {
+          return this.prisma.appUser.create({
+            data: {
+              email: "to@keep.de",
+              firstname: "To",
+              lastname: "Keep",
+            },
+          });
+        }
+      }
+
+      const toTest = new TestClass(prismaClient);
+
+      await toTest.createUserWithAdditionalUser().catch(async (err) => {
+        // console.log(queryEvents.map((q) => q.query));
+        expect(err.message).toBe("some error occured");
+        expect(queryEvents[0].query).toBe("BEGIN");
+        expect(queryEvents[1].query).toContain("INSERT");
+        expect(queryEvents[2].query).toContain("SELECT");
+
+        // REQUIRES_NEW transactions
+        expect(queryEvents[4].query).toBe("BEGIN");
+        expect(queryEvents[5].query).toContain("INSERT");
+        expect(queryEvents[6].query).toContain("SELECT");
+        expect(queryEvents[7].query).toContain("COMMIT");
+
+        expect(queryEvents[8].query).toBe("BEGIN");
+        expect(queryEvents[9].query).toContain("INSERT");
+        expect(queryEvents[10].query).toContain("SELECT");
+        expect(queryEvents[11].query).toBe("COMMIT");
+
+        //  the new transaction to keep even when first transaction apply
+        expect(queryEvents[12].query).toBe("ROLLBACK");
+        expect(queryEvents.length).toBe(13);
 
         expect(await prismaClient.appUser.count()).toBe(1);
         const user = await prismaClient.appUser.findFirst();
